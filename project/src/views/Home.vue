b68a52aa4a760372ececcf2a91c95a37b8c83a45
ThomasMarinaro <90782144+ThomasMarinaro@users.noreply.github.com>
2022-02-14 10:57:41 -0500

    grsgasedgedszh

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="home">
- <h1>!! make your monos!!</h1>  
-
+ <h1>the proverbial door</h1> 
+ <img src="https://media2.giphy.com/avatars/julianeasily/XgpXIhg4pL5V.gif" alt="someone stole this cat">
+  <h2>welcome to my store  </h2>
   </div>
 
 </template>
-<template>   
-<div class="box">
-<h1> text</h1>
-</div>
+
   </template>
 <script>
 
