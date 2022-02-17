5814ee8b1f0aec966b32df02d4abdaa26144f874
ThomasMarinaro <thomasmarinaro9@gmail.com>
2022-02-16 21:30:03 -0500

    Update main.js

--- project/src/main.js
+++ project/src/main.js
@@ -11,16 +11,3 @@ new Vue({
 
 
 
-var myHeaders = new Headers();
-myHeaders.append("X-eBirdApiToken", "{{x-ebirdapitoken}}");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("https://api.ebird.org/v2/data/obs/KZ/recent", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
