768fec94b9e69ca1325dd34f78c236de4b87f20e
ThomasMarinaro <90782144+ThomasMarinaro@users.noreply.github.com>
2022-02-18 10:57:21 -0500

    aSFWQQFq

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -5,13 +5,20 @@
   <h2>welcome to my store  </h2>
 
   <h1> Buy my cars</h1>
-  <div class="items">
+  <div class="things">
+  <div class="items" id="rs200">
     <h1> ford rs200 </h1>
     <p> this beaut is packing a cosworth engine so good i should not be selling </p>
     <img src="https://images.cdn.circlesix.co/image/1/700/0/uploads/posts/2016/03/f4c48dff4244772e8c9562f694204a02.jpg" alt="rs200"> 
 
   </div>
+   <div class="items" id="audi">
+    <h1> the audi quattro </h1>
+    <p> The german group b machine sometimes they can have a little fun </p>
+    <img src="https://c4.wallpaperflare.com/wallpaper/373/72/25/audi-ur-quattro-audi-drifting-wallpaper-preview.jpg" alt="rs200">
 
+  </div>
+</div>
 
   </div>
 
@@ -19,6 +26,21 @@
 
 
 <script>
+
 </script>
 
+<style>
+.things{
+display: flex;
+flex-direction: column;
 
+background-repeat:true;
+}
+#rs200{
+  background: crimson;
+}
+#audi{
+background: darkorange;
+
+}
+</style>
