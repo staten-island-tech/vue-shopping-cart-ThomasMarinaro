526a00d807e275a9d0b71555533d7049c5ee3d20
ThomasMarinaro <90782144+ThomasMarinaro@users.noreply.github.com>
2022-03-02 10:56:43 -0500

    aewfewaf
    rqr

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -5,23 +5,36 @@
   <h2>welcome to my store  </h2>
 
   <h1> Buy my cars</h1>
-  <div class="things">
-  <div class="items" id="rs200">
+ 
+  <div class="things">  
+    <div class="items" id="ferrari">
+    <h1> The F40 competizione</h1>
+    <p>this ferrari is not for keeping in a garage but it has been let this caged tiger out</p>
+    <img src="https://www.supercars.net/blog/wp-content/uploads/2019/10/JcQ9GNJ7REKQFiVJzXmmTA.jpg" alt="ferrari">
+    </div>
+    <div class="items" id="rs200">
     <h1> ford rs200 </h1>
     <p> this beaut is packing a cosworth engine so good i should not be selling </p>
     <img src="https://images.cdn.circlesix.co/image/1/700/0/uploads/posts/2016/03/f4c48dff4244772e8c9562f694204a02.jpg" alt="rs200"> 
+</div>
 
-  </div>
    <div class="items" id="audi">
     <h1> the audi quattro </h1>
     <p> The german group b machine sometimes they can have a little fun </p>
     <img src="https://c4.wallpaperflare.com/wallpaper/373/72/25/audi-ur-quattro-audi-drifting-wallpaper-preview.jpg" alt="rs200">
-
   </div>
-</div>
-
+  <div class="items" id="carrera">
+    <h1> The 1973 911 carrera</h1>
+    <p>this porsche is ready to rumble with a classic livery and a tried and true 911 body</p>
+    <img src="https://silodrome.com/wp-content/uploads/2017/08/Porsche-911-Carrera-RS-2.7-1480x988.jpg" alt="carrera">
+    </div>
+    <div class="items" id="celica">
+    <h1> The 1994 celica </h1>
+    <p>this toyota beauty proved 10 years later rally was not dead</p>
+    <img src="https://i.pinimg.com/originals/5f/3f/46/5f3f4694f56c5f3a40319df79eb7d399.jpg" alt="celica">
+    </div> 
   </div>
-
+ </div>
 </template>
 
 
@@ -30,17 +43,53 @@
 </script>
 
 <style>
+.home{
+  
+
+}
 .things{
+  display: flex;
+  flex-direction: row;
+  margin-right: 100px;
+  margin-left: 100px;
+  flex-grow:1;
+  flex-wrap: wrap;
+}
+.items{
 display: flex;
 flex-direction: column;
+flex-wrap: wrap;
 
 background-repeat:true;
+flex-grow: 1;
+flex-shrink:1;
+scale:0.8;
+align-items: flex-start;
 }
+img{
+padding:30px; 
+flex-shrink: 1;
+max-width: 40em;
+}
+
 #rs200{
   background: crimson;
 }
 #audi{
 background: darkorange;
+
+}
+#carrera{
+background-color: rgb(255, 208, 0);
+
+}
+#ferrari{
+background-color: rgb(206, 0, 0);
+
+}
+#celica{
+background-color: rgb(110, 223, 5);
+
 
 }
 </style>
