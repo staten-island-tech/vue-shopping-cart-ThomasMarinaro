49057cddeec681fc069573db5ae4c462e39f4451
ThomasMarinaro <thomasmarinaro9@gmail.com>
2022-03-08 18:39:14 -0500

    Update About.vue
    ih;iohoj';l

--- project/src/views/About.vue
+++ project/src/views/About.vue
@@ -2,23 +2,37 @@
   <div class="about">
     <h1>fake cars </h1>
  <body>
-  <h1>gta cars </h1>
-  <div class="btn">  
-    <button id="setting" type="button"> setting</button>
-    <button id="expensive" type="button" > The Land of Price </button>
-    <button id="sportsclassic" type="button">Sportsclassics</button>
-    <button id="sports" type="button">Sports</button>
-    <button id="Offroad" type="button">Offroad</button>
-  </div>
+  <h1>game cars </h1>
+  
+  
+ 
     <section class="box" id="box"> 
-    
+       <ul>
+      <li v-for="car in cars" :key="car">{{ car }}</li>
+    </ul>
 
     </section>
  </body>
   </div>
 </template>
 <script>
+export default {
+  name: "about",
+  components: {},
+  data(){
+    return{
+      cars: [
+        {
+          name:"calicogtf",
+          location:"SSAA",
+        }]
+        
+    };
+  
+  }
 
+
+}
 
 
 
